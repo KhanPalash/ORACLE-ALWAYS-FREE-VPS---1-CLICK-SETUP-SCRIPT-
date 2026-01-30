dbd4b3d3aa933a7f058a917ec2e1f41a4ca9ffff
KhanPalash <81261640+KhanPalash@users.noreply.github.com>
2026-01-31 03:59:34 +0600

    Delete oracle-vps.sh

--- oracle-vps.sh
+++
@@ -1,6 +0,0 @@
-# 1. Script download
-curl -fsSL https://raw.githubusercontent.com/ORACLE-ALWAYS-FREE-VPS---1-CLICK-SETUP-SCRIPT-/oracle-vps-setup/main/setup.sh -o oracle-vps.sh
-
-# 2. Execute (Local PC তে)
-chmod +x oracle-vps.sh
-./oracle-vps.sh
